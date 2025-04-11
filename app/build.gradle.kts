5ea3768d9719b8ac68f91b2d65675bbf1a8cf680
kolom1234 <kolom1357@gmail.com>
2025-04-11 16:57:43 +0900

    텍스트 및 음성 생성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,4 +117,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    implementation ("androidx.room:room-runtime:2.5.0")
+    kapt ("androidx.room:room-compiler:2.5.0")
+    implementation ("androidx.room:room-ktx:2.5.0")
 }
